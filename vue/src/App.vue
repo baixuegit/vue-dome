df5bdf5a024fad822e0994fcaec43853586c93c3
柏雪 <angellbaixue@163.com>
2019-01-08 18:06:40 +0800

    修改table样式和flex布局

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+     <router-view v-if="isRouterAlive"></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  provide () { // 注册一个方法
+    return {
+      reload: this.reload
+    }
+  },
+  data () {
+    return {
+      isRouterAlive: true
+    }
+  },
+  methods: {
+    reload () {
+      this.isRouterAlive = false
+      this.$nextTick(function () {
+        this.isRouterAlive = true
+      })
+    }
+  }
 }
 </script>
 
@@ -17,6 +35,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 40px;
+  display: flex;
+  flex: 1;
+  flex-direction:column ;
+}
+html,body{
+  flex-direction:column ;
+  width: 100%;
+  height: 100%;
+  display: flex;
 }
 </style>
--- vue/src/views/index/my_index.vue
+++ vue/src/views/index/my_index.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="domeWapper">
+  <div class="domeWapper flex_div flex_auto">
     <div class="btnWapper">
+      <el-button type="primary" @click="refresh" size="small">刷新</el-button>
       <el-button type="primary" @click="mixniClick" size="small">点击查询</el-button>
       <el-button type="primary" @click="exportExcel" size="small">导出数据</el-button>
     </div>
-    <el-table
-    v-loading="loading"
-    :data="tableData"
-    border
-    stripe
-    style="width: 100%">
-      <el-table-column
-        align="center"
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
+    <div class="tableWaper flex_div flex_auto">
+      <el-table class="flex_div flex_auto"
+      v-loading="loading"
+      :data="tableData"
+      border
+      stripe
+      style="">
+        <el-table-column
+          align="center"
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          align="center"
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 <script>
@@ -36,6 +39,7 @@ import FileSaver from 'file-saver'
 import XLSX from 'xlsx'
 export default {
   // mixins: [mixin],
+  inject: ['reload'], // 引入方法
   data () {
     return {
       loading: false,
@@ -44,6 +48,7 @@ export default {
     }
   },
   created () {
+    this.mixniClick()
     this.hello()// mixins混入的方法
   },
   mounted () {},
@@ -61,18 +66,71 @@ export default {
         FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), 'sheetjs.xlsx')
       } catch (e) { if (typeof console !== 'undefined') console.log(e, wbout) }
       return wbout
+    },
+    refresh () {
+      this.reload() // 调用方法
+      // console.log('刷新')
     }
   }
 }
 </script>
 <style>
 .domeWapper {
+  flex-direction:column ;
+  /* height: 50%; */
   /* width: 100%; */
-  min-width: 600px;
+  /* min-width: 600px; */
   padding:0 40px 40px 40px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 }
 .btnWapper{
+  /* display: flex; */
   padding-bottom: 20px;
   text-align: right;
+  width: 100%;
 }
+.tableWaper{
+  /* display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 100%; */
+
+}
+.flex_div {
+  display: flex;
+  flex-direction: column;
+}
+.flex_div .flex_auto {
+  flex: 1;
+  overflow: auto;
+}
+.el-table::before{
+  height: 0 !important;
+}
+.el-table__body-wrapper{
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: auto;
+}
+.el-table__body{
+  /* display: flex; */
+  /* flex-direction: column; */
+   flex: 1;
+  overflow: auto;
+}
+
+/* 修改滚动条的样式 */
+/* ::-webkit-scrollbar {
+  background: rgba(0, 0, 0, 0);
+  width: 7px;
+  height: 7px;
+}
+::-webkit-scrollbar-thumb {
+  background: rgba(37, 36, 36, 0.27);
+  border-radius: 10px;
+} */
+
 </style>
